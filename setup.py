c03e66306c0367e8833034d851de6c9ba4211d07
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-09-11 11:21:42 -0700

    Updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sentry_campfire',
-    version='0.1.1',
+    version='0.1.2',
     author='Mustafa Khattab',
     author_email='mustafa.khattab@gmail.com',
     url='https://github.com/mkhattab/sentry-campfire',
